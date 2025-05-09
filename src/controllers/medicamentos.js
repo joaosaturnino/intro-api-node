3f5408adffe26acf2efcbfb6d5c016910f42e872
joaosaturnino <joao.saturnino@etec.sp.gov.br>
2025-05-08 21:48:58 -0300

    alterações

--- src/controllers/medicamentos.js
+++ src/controllers/medicamentos.js
@@ -1,60 +1,59 @@
-const db = require('../dataBase/connection');
-const { json, response } = require('express');
+const db = require("../dataBase/connection");
+const { json, response } = require("express");
 
-//var fs = require('fs-extra');
+var fs = require("fs-extra");
 
-function geraUrl (e) {
-
-  //garantir que valores em branco carreguem algo
-  let img = med_img ? med_img : 'sem_imagem.jpg';
-  // verifica se imagem existe
-  if (!fs.esxistsSync('./public/upload/medicamentos/' + img)) {
-    img = 'sem_imagem.jpg';
+function geraUrl(e) {
+  // garantir que os valores em branco carreguem algo
+  let img = e.med_img ? e.med_img : "sem.jpg";
+  //verifica se imagem existe
+  if (!fs.existsSync("./upload/medicamentos/" + img)) {
+    img = "sem.jpg";
   }
 
-  const medicamentos = {
-    med_id,
-    med_nome,
-    med_dosagem,
-    med_quantidade,
-    forma_id,
-    descricao,
-    lab_id,
-    med_img: 'http://192.168.200.27/upload/medicamentos/' + img,
-    tipo_id,
-    
-  }
-  return medicamentos;
-  
+  const medicamento = {
+    med_id: e.med_id,
+    med_nome: e.med_nome,
+    med_dosagem: e.med_dosagem,
+    med_quantidade: e.med_quantidade,
+    forma_id: e.forma_id,
+    descricao: e.descricao,
+    lab_id: e.lab_id,
+    med_img: "http://192.168.158.164:3333/upload/medicamentos/" + img, //+ img,
+    tipo_id: e.tipo_id,
+  };
+  return medicamento;
 }
 
 // Controller para gerenciar medicamentos
 // Este módulo contém funções para listar, cadastrar, editar e apagar medicamentos no banco de dados
 module.exports = {
-
   //Listar todos os medicamentos
   async listarMedicamentos(request, response) {
     try {
       // indtrução sql para listar medicamentos
       const sql = `SELECT 
       med_id, med_nome, med_dosagem, med_quantidade, 
-      forma_id descricao, lab_id, med_img, tipo_id 
+      forma_id, descricao, lab_id, med_img, tipo_id 
       FROM medicamento;`;
       // executa a instrução de listagem no banco de dados
       const [rows] = await db.query(sql);
+
+      //chamada para montar url
+      const resultado = medicamento[0].map(geraUrl);
       // exibe o resultado da consulta
       return response.status(200).json({
         sucesso: true,
-        mensagem: 'Lista de medicamentos',
+        mensagem: "Lista de medicamentos",
         itens: rows.length,
-        dados: rows
+        dados: resultado,
       });
       // retorna erro caso ocorra
-    }catch (error) {
+    } catch (error) {
       return response.status(500).json({
         sucesso: false,
-        mensagem: 'Erro na requisição.',
-        dados: error.mensage
+        mensagem: "Erro na requisição.",
+        dados: error.mensage,
       });
     }
   },
@@ -97,26 +96,43 @@ module.exports = {
   async cadastrarMedicamentos(request, response) {
     try {
       // parametros passados via corpo de requisição
-      const { med_nome, med_dosagem, med_quantidade, forma_id, lab_id, med_img, tipo_id } = request.body;
+      const {
+        med_nome,
+        med_dosagem,
+        med_quantidade,
+        forma_id,
+        lab_id,
+        med_img,
+        tipo_id,
+      } = request.body;
       // instrução sql para inserção
-      const sql = 'INSERT INTO medicamento (med_nome, med_dosagem, med_quantidade, forma_id, lab_id, med_img, tipo_id) VALUES (?, ?, ?, ?, ?, ?, ?);';
+      const sql =
+        "INSERT INTO medicamento (med_nome, med_dosagem, med_quantidade, forma_id, lab_id, med_img, tipo_id) VALUES (?, ?, ?, ?, ?, ?, ?);";
       // definição de array com paramentros que receberão os valores do front-end
-      const values = [med_nome, med_dosagem, med_quantidade, forma_id, lab_id, med_img, tipo_id];
+      const values = [
+        med_nome,
+        med_dosagem,
+        med_quantidade,
+        forma_id,
+        lab_id,
+        med_img,
+        tipo_id,
+      ];
       // executa a instrução de inserção no banco de dados
       const [rows] = await db.query(sql, values);
       // exibe o id do registro inserido
       return response.status(201).json({
         sucesso: true,
-        mensagem: 'Medicamento cadastrado com sucesso.',
+        mensagem: "Medicamento cadastrado com sucesso.",
         itens: rows.length,
-        dados: rows
+        dados: rows,
       });
       // retorna erro caso ocorra
     } catch (error) {
       return response.status(500).json({
         sucesso: false,
-        mensagem: 'Erro na requisição.',
-        dados: error.mensage
+        mensagem: "Erro na requisição.",
+        dados: error.mensage,
       });
     }
   },
@@ -125,28 +141,46 @@ module.exports = {
   async editarMedicamentos(request, response) {
     try {
       // parametros passados via corpo de requisição
-      const { med_nome, med_dosagem, med_quantidade, forma_id, lab_id, med_img, tipo_id } = request.body;
+      const {
+        med_nome,
+        med_dosagem,
+        med_quantidade,
+        forma_id,
+        lab_id,
+        med_img,
+        tipo_id,
+      } = request.body;
       // parametros passados via url
       const { med_id } = request.params;
       // instrução sql para edição
-      const sql = 'UPDATE medicamento SET med_nome = ?, med_dosagem = ?, med_quantidade = ?, forma_id = ?, lab_id = ?, med_img = ?, tipo_id = ? WHERE med_id = ?;';
+      const sql =
+        "UPDATE medicamento SET med_nome = ?, med_dosagem = ?, med_quantidade = ?, forma_id = ?, lab_id = ?, med_img = ?, tipo_id = ? WHERE med_id = ?;";
       // definição de array com paramentros que receberão os valores do front-end
-      const values = [med_nome, med_dosagem, med_quantidade, forma_id, lab_id, med_img, tipo_id, med_id];
+      const values = [
+        med_nome,
+        med_dosagem,
+        med_quantidade,
+        forma_id,
+        lab_id,
+        med_img,
+        tipo_id,
+        med_id,
+      ];
       // executa a instrução de edição no banco de dados
       const [rows] = await db.query(sql, values);
       // exibe o resultado da consulta
       return response.status(200).json({
         sucesso: true,
-        mensagem: 'Medicamento editado com sucesso.',
+        mensagem: "Medicamento editado com sucesso.",
         itens: rows.length,
-        dados: rows
+        dados: rows,
       });
-    // retorna erro caso ocorra
+      // retorna erro caso ocorra
     } catch (error) {
       return response.status(500).json({
         sucesso: false,
-        mensagem: 'Erro na requisição.',
-        dados: error.mensage
+        mensagem: "Erro na requisição.",
+        dados: error.mensage,
       });
     }
   },
@@ -157,7 +191,7 @@ module.exports = {
       // parametros passados via url
       const { med_id } = request.params;
       // instrução sql para apagar medicamento
-      const sql = 'DELETE FROM medicamento WHERE med_id = ?;';  
+      const sql = "DELETE FROM medicamento WHERE med_id = ?;";
       // definição de array com paramentros que receberão os valores do front-end
       const values = [med_id];
       // executa a instrução de apagar no banco de dados
@@ -165,16 +199,16 @@ module.exports = {
       // exibe o resultado da consulta
       return response.status(200).json({
         sucesso: true,
-        mensagem: 'Medicamento apagado com sucesso.',
+        mensagem: "Medicamento apagado com sucesso.",
         itens: rows.length,
-        dados: rows
+        dados: rows,
       });
-    // retorna erro caso ocorra
+      // retorna erro caso ocorra
     } catch (error) {
       return response.status(500).json({
         sucesso: false,
-        mensagem: 'Erro na requisição.',
-        dados: error.mensage
+        mensagem: "Erro na requisição.",
+        dados: error.mensage,
       });
     }
   },
@@ -206,4 +240,4 @@ module.exports = {
   //     });
   //   }
   // }
-}
+};
